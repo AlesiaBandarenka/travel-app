6ffdea4255ba6b92692d93cb5b50f935b8674c67
Alesia Bandarenka <happylesechka@gmail.com>
2023-03-30 13:17:54 +0200

    Add dist

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/travel-app/' : '/',
   filenameHashing: false,
   productionSourceMap: false,
   configureWebpack: {
